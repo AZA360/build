814a70256a6a8dd793edf33bff25a240dff9a5aa
ehmicky <ehmicky@gmail.com>
2020-06-05 21:40:43 +0200

    Lazy load `utils`

--- packages/build/src/plugins/child/utils.js
+++ packages/build/src/plugins/child/utils.js
@@ -1,36 +1,50 @@
-const cacheUtils = require('@netlify/cache-utils')
-const functionsUtils = require('@netlify/functions-utils')
-const gitUtils = require('@netlify/git-utils')
-const runUtils = require('@netlify/run-utils')
-
 const { failBuild, failPlugin, cancelBuild } = require('../error')
 
-const { getStatusUtil } = require('./status')
+const { addLazyProp } = require('./lazy')
+const { show } = require('./status')
 
 // Some utilities need to perform some async initialization logic first.
 // We do it once for all plugins in the parent process then pass it to the child
 // processes.
 const startUtils = async function(buildDir) {
+  const gitUtils = require('@netlify/git-utils')
   const git = await gitUtils({ cwd: buildDir })
   return { git }
 }
 
 // Retrieve the `utils` argument.
 const getUtils = function({ utilsData: { git }, constants: { FUNCTIONS_SRC, CACHE_DIR }, runState }) {
-  const buildUtils = { failBuild, failPlugin, cancelBuild }
-  const gitA = gitUtils.load(git)
-  const cache = cacheUtils.bindOpts({ cacheDir: CACHE_DIR })
+  const build = { failBuild, failPlugin, cancelBuild }
+  const utils = { build }
+  addLazyProp(utils, 'git', getGitUtils.bind(null, git))
+  addLazyProp(utils, 'cache', getCacheUtils.bind(null, CACHE_DIR))
+  addLazyProp(utils, 'run', getRunUtils)
+  addLazyProp(utils, 'functions', getFunctionsUtils.bind(null, FUNCTIONS_SRC))
+  addLazyProp(utils, 'status', getStatusUtils.bind(null, runState))
+  return utils
+}
+
+const getGitUtils = function(git) {
+  return require('@netlify/git-utils').load(git)
+}
+
+const getCacheUtils = function(CACHE_DIR) {
+  const cacheUtils = require('@netlify/cache-utils')
+  return cacheUtils.bindOpts({ cacheDir: CACHE_DIR })
+}
+
+const getRunUtils = function() {
+  return require('@netlify/run-utils')
+}
+
+const getFunctionsUtils = function(FUNCTIONS_SRC) {
+  const functionsUtils = require('@netlify/functions-utils')
   const add = src => functionsUtils.add(src, FUNCTIONS_SRC, { fail: failBuild })
-  const functions = { add }
-  const status = getStatusUtil(runState)
-  return {
-    build: buildUtils,
-    git: gitA,
-    cache,
-    run: runUtils,
-    functions,
-    status,
-  }
+  return { add }
+}
+
+const getStatusUtils = function(runState) {
+  return { show: show.bind(undefined, runState) }
 }
 
 module.exports = { startUtils, getUtils }
--- packages/build/tests/plugins/utils/tests.js
+++ packages/build/tests/plugins/utils/tests.js
@@ -3,32 +3,24 @@ const test = require('ava')
 const { removeDir } = require('../../helpers/dir')
 const { runFixture, FIXTURES_DIR } = require('../../helpers/main')
 
-test('utils-load none', async t => {
-  await runFixture(t, 'none')
+test('Expose some utils', async t => {
+  await runFixture(t, 'keys')
 })
 
-test('utils-load sync function', async t => {
-  await runFixture(t, 'function_sync')
+test('Utils are defined', async t => {
+  await runFixture(t, 'defined')
 })
 
-test('utils-load async function', async t => {
-  await runFixture(t, 'function_async')
-})
-
-test('utils-load error', async t => {
-  await runFixture(t, 'error', { env: { CACHED_COMMIT_REF: 'aaaaaaaa' } })
-})
-
-test('utils functions', async t => {
+test('Can run utils', async t => {
   await removeDir(`${FIXTURES_DIR}/functions/functions`)
   await runFixture(t, 'functions')
   await removeDir(`${FIXTURES_DIR}/functions/functions`)
 })
 
-test('utils git with no root', async t => {
+test('Git utils fails if no root', async t => {
   await runFixture(t, 'git_no_root', { copyRoot: { git: false } })
 })
 
-test('utils git with no root delayed error', async t => {
-  await runFixture(t, 'git_no_root_delayed', { copyRoot: { git: false } })
+test('Git utils does not fail if no root and not used', async t => {
+  await runFixture(t, 'keys', { copyRoot: { git: false } })
 })
